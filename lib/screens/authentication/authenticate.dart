8b3f9250eef2aa01d37a48a32741d669bbd738dd
Bhuva Pinak <69301923+Pinak0910@users.noreply.github.com>
2020-10-09 23:41:36 +0530

    Create authenticate.dart

---
+++ lib/screens/authentication/authenticate.dart
@@ -0,0 +1,26 @@
+import 'register.dart';
+import 'signin.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+
+  void toggleView() {
+    // !showSignIn toggles the value
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
+  }
+}
